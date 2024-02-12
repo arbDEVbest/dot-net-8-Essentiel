32f8ec5a1f68cafd619d0c4ea864c81ff5ddd48d
Fethi ARBOUZ <70934728+arbDEVbest@users.noreply.github.com>
2024-02-12 18:27:11 +0000

    Compile Regex pattern

--- src/Program.cs
+++ src/Program.cs
@@ -1,13 +1,18 @@
-﻿namespace ExpReg;
+﻿using System.Text.RegularExpressions;
 
-class Program
+namespace ExpReg;
+
+partial class Program
 {
+    [GeneratedRegex(@"#\w\w+")]
+    private static partial Regex HashTagRegEx();
     static void Main(string[] args)
     {
+        
         var bic = "ATCICIAB";
 
         Console.Write($"Le BIC '{bic}' est ");
-        if (bic is not null) // TODO : [A-Z]{6}[A-Z0-9]{2,5}
+        if (Regex.IsMatch(bic, "[A-Z]{6}[A-Z0-9]{2,5}")) // TODO : [A-Z]{6}[A-Z0-9]{2,5}
         {
             Console.WriteLine("valide.");
         }
@@ -18,14 +23,16 @@ class Program
 
         var twitGeorgeSand = "Tu fuyais la #solitude et la trouvait #partout.";
 
-        var hashtags = Array.Empty<string>(); // TODO : Extraire #\w+
+        var hashtags = HashTagRegEx().Matches(twitGeorgeSand); // TODO : Extraire #\w+
 
         foreach (var tag in hashtags)
         {
             Console.WriteLine(tag);
         }
 
-        var html = twitGeorgeSand;
+        var html = HashTagRegEx().Replace(
+            twitGeorgeSand, m => $"<a href='?tag={m.Value.Substring(1)}'>{m.Value}</a>"
+        );
 
         Console.WriteLine(html);
     }
